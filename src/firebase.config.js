7bdf9426ab6aec0a431af18b7f98951e2bc5b407
saurab77 <romanmilan710@gmail.com>
2024-04-11 21:08:07 +0545

    amazone clone

---
+++ src/firebase.config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDEAS61Jxh-7BaoW55skFPp7ZOVA8qjrsA",
+  authDomain: "clone-f8e52.firebaseapp.com",
+  projectId: "clone-f8e52",
+  storageBucket: "clone-f8e52.appspot.com",
+  messagingSenderId: "141594076114",
+  appId: "1:141594076114:web:5d5bad3556c9a58ceb1e0b",
+  measurementId: "G-3EXECHBY09"
+};
+
+
+export const app = initializeApp(firebaseConfig);
+export default firebaseConfig;
